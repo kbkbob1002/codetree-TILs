ca79ac308c000a4c8fe27b9769f3fd30cbe27711
bo_gr2 <84168583+kbkbob1002@users.noreply.github.com>
2024-08-12 19:06:08 +0900

    Title: 합과 차의 나눗셈, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240812/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
+++ 240812/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int a,b;
-
+    double c;
     cin >> a >> b;
 
-    cout.precision(2);
-    cout << (a+b)/(a-b);
+    cout.precision(3);
+    c= double(a+b) / (a-b);
+    cout << c;
     return 0;
 }
