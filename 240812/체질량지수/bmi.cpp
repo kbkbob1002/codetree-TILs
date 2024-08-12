5ff1720f864a7c24173f5568d5f07900f171aa66
bo_gr2 <84168583+kbkbob1002@users.noreply.github.com>
2024-08-12 20:39:01 +0900

    Title: 체질량지수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240812/체질량지수/bmi.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int h,w;
+    int b;
+    cin >> h >> w ;
+    b= (10000*w)/(h*h);
+
+    if (b>=25) {
+        cout << b << endl;
+        cout <<"Obesity";
+    }
+    else {
+        cout <<b;
+    
+    }
+
+
+
+    return 0;
+}
