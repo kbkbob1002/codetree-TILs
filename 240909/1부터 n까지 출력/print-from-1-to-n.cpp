fa3cd276d88b8f77a812e387741eed240092e38a
bo_gr2 <84168583+kbkbob1002@users.noreply.github.com>
2024-09-09 23:36:53 +0900

    Title: 1부터 n까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240909/1부터 n까지 출력/print-from-1-to-n.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    cin >> a;
+    for (int i =1; i<=a; i++) {
+        cout << i;
+        cout << " ";
+    }
+    return 0;
+}
