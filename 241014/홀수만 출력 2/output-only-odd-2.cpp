a8db47e55ebc3e78ad4bb318f6cf11159c3ba775
bo_gr2 <84168583+kbkbob1002@users.noreply.github.com>
2024-10-14 16:50:59 +0900

    Title: 홀수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241014/홀수만 출력 2/output-only-odd-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int b,a;
+    cin >> b >> a ;
+
+    for (int i=b; i>=a; i--)
+    if(i % 2 == 1) {
+            cout << i << " ";
+
+    }
+
+
+    return 0;
+}
