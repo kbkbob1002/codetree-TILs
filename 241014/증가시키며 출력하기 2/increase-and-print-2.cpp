ff2e857724ffbe94326407a65747d173ab37b557
bo_gr2 <84168583+kbkbob1002@users.noreply.github.com>
2024-10-14 17:14:05 +0900

    Title: 증가시키며 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241014/증가시키며 출력하기 2/increase-and-print-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int i = 10;            // <- 시작 값(초기화식)
+
+		while (i <= 26) {      // <-계속 반복할 조건(조건식)
+			cout << i << " ";    //<-원하는 코드 작성
+			i++;                
+		}
+
+
+	return 0;
+
+}
